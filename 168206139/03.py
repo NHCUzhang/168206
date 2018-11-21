58d17911c4fadf26d4babdb303c17f12242ed70c
NHCUzhang <44492468+NHCUzhang@users.noreply.github.com>
2018-11-21 20:25:08 +0800

    Update 03.py

--- 168206139/03.py
+++ 168206139/03.py
@@ -1,28 +1,28 @@
-def merge(a,b):
-    c=[]
-    j=h=0
-    while j<len(a) and h<len(b):
-        if a[j]<b[h]:
-            c.append(a[j])
-            j=j+1
-        else:
-            c.append(b[h])
-            h=h+1
-    if j==len(a):
-        for i in b[h:]:
-            c.append(i)
-    else:
-        for i in a[j:]:
-            c.append(i)
-    return c
-    
-def mergeSort(list_1):
-    if len(list_1)<=1:
-        return list_1
-    middle=len(list_1)//2
-    left = mergeSort(list_1[:middle])
-    right = mergeSort(list_1[middle:])
+def MergeSort(alist):
+    if len(alist) <= 1:
+        return alist
+    middle = len(alist) // 2
+    left = MergeSort(alist[:middle])
+    right = MergeSort(alist[middle:])
     return merge(left, right)
 
-test_list=[8,5,4,1,3,9,7]
-print(mergeSort(test_list))
+def merge(a,b):
+    c = []
+    d = e = 0
+    while d < len(a) and e < len(b):
+        if a[d] < b[e]:
+            c.append(a[d])
+            d += 1
+        else:
+            c.append(b[e])
+            e += 1
+    if d == len(a):
+        for i in b[e:]:
+            c.append(i)
+    else:
+        for i in a[d:]:
+            c.append(i)
+    return c
+
+list = [8,5,4,1,3,9,7]
+print(MergeSort(list))
